703273e982bbb2fb83f4c885bff90e0dbb25e407
Gautam <gautambhatoy1234@gmail.com>
2020-05-22 23:16:12 -0700

    Java.util imported and user prompted to enter string

--- MyJavaProject/src/myAppTester.java
+++ MyJavaProject/src/myAppTester.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -11,9 +13,22 @@ public class myAppTester {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static void main(String[] args)
+	 {
+	 System.out.println("Enter string to reverse:");
+	 
+	 Scanner read = new Scanner(System.in);
+	 String str = read.nextLine();
+	 
+	 StringBuilder sb = new StringBuilder();
+	 
+	 for(int i = str.length() - 1; i >= 0; i--)
+	 {
+	 sb.append(str.charAt(i));
+	 }
+	 
+	 System.out.println("Reversed string is:");
+	 System.out.println(sb.toString());
+	 }
 
 }
