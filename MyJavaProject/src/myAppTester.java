4078b1e8e1d5c994b8954f73a0f10c739d8cee03
Gautam <gautambhatoy1234@gmail.com>
2020-05-22 23:10:22 -0700

    Class created

---
+++ MyJavaProject/src/myAppTester.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author 12368
+ *
+ */
+public class myAppTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
